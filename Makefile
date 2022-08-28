908bad0d2342e9a0f33ec3e8d660d75bd88e6713
keinuma <nununu.mono@gmail.com>
2022-08-28 15:21:23 +0900

    Activate API mode to Rails

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+.PHONY: install
+install:
+	bundle install
+
 .PHONY: test
 test:
 	bundle exec rspec
@@ -5,3 +9,11 @@ test:
 .PHONY: console
 console:
 	rails console --sandbox
+
+.PHONY: create-db
+create-db:
+	rake db:create
+
+.PHONY: migrate
+migrate:
+	rake db:migrate
--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,11 @@ Bundler.require(*Rails.groups)
 
 module RailsStarter
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.api_only = true
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware
   end
 end
